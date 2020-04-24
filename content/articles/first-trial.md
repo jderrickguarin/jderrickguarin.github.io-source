617f31d423ff1ea23ab4ecc5fb8415397d829695
Derrick Guarin <derrickguarin@gmail.com>
2020-04-24 23:07:31 +0800

    added directories

---
+++ content/articles/first-trial.md
@@ -0,0 +1,12 @@
+Title: First Trial
+Date: 2020-4-24 10:30
+Modified: 2020-4-24 10:30
+Category: Personal
+Tags: pelican, publishing
+Slug: first-trial
+Authors: Derrick Guarin
+Summary: Trial webpage 
+
+
+# Welcome to Tinkering Theodoric
+This is my personal website. This site is under construction.
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,11 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Derrick'
-SITENAME = 'jderrickguarin'
+SITENAME = 'Tinkering Theodoric'
+SITESUBTITLE = u'Contains musings on data science, economics, econometrics, computer science and beyond'
 SITEURL = ''
-
 PATH = 'content'
-
 TIMEZONE = 'Asia/Manila'
-
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -33,3 +31,28 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+
+PLUGINS = ['ipynb.liquid',  'liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# Theme settings
+THEME = './theme/'
+
+ABOUT_PAGE = '/pages/about.html'
+TWITTER_USERNAME = 'JDerrickG'
+GITHUB_USERNAME = 'jderrickguarin'
+AUTHOR_BLOG = 'http://jderrickguarin.github.io'
+AUTHOR_CV = '' 
+SHOW_ARCHIVES = False
+SHOW_FEED = False 
+
+ENABLE_MATHJAX = True
+
+STATIC_PATHS = ['images', 'figures', 'downloads']
